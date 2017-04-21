feb0484c75ec18c9e1ae16387e6188e562f1de62
Katherine Matthews <katherinematthews.polsci@gmail.com>
2017-04-21 14:05:09 -0700

    almost done.

--- src/GummiBearKingdom/Startup.cs
+++ src/GummiBearKingdom/Startup.cs
@@ -36,6 +36,9 @@ namespace GummiBearKingdom
 
         public void Configure(IApplicationBuilder app)
         {
+            var context = app.ApplicationServices.GetService<GummiBearKingdomContext>();
+            AddTestData(context);
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
@@ -45,10 +48,60 @@ namespace GummiBearKingdom
 
             app.UseStaticFiles();
 
-            app.Run(async (context) =>
+            app.Run(async (context1) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context1.Response.WriteAsync("Hello World!");
             });
+        }
+
+        private static void AddTestData(GummiBearKingdomContext context)
+        {
+            var testProduct1 = new Models.Product
+            {
+                ProductName = "Sour Grape",
+                ProductCost = "9.99",
+                ProductCountry = "USA"
+
+            };
+            context.Products.Add(testProduct1);
+
+            var testProduct2 = new Models.Product
+            {
+                ProductName = "Cadbury Dairy Milk",
+                ProductCost = "4.99",
+                ProductCountry = "UK"
+
+            };
+            context.Products.Add(testProduct2);
+
+            var testProduct3 = new Models.Product
+            {
+                ProductName = "Elite Milk Chocolate Bar with Popping Candies",
+                ProductCost = "7.00",
+                ProductCountry = "Israel"
+
+            };
+            context.Products.Add(testProduct3);
+
+            var testProduct4 = new Models.Product
+            {
+                ProductName = "White Rabbit Creamy Candy",
+                ProductCost = "12.50",
+                ProductCountry = "USA"
+
+            };
+            context.Products.Add(testProduct4);
+
+            var testProduct5 = new Models.Product
+            {
+                ProductName = "Chimes Ginger Chews",
+                ProductCost = "3.09",
+                ProductCountry = "Indonesia"
+
+            };
+            context.Products.Add(testProduct5);
+
+            context.SaveChanges();
         }
     }
 }
